be674b3b74c91b57c35e41639e02762b84fe59b8
vladnov138 <vladnov138@gmail.com>
2024-10-27 23:52:53 +0800

    add validation

--- src/logics/warehouse_transaction_prototype.py
+++ src/logics/warehouse_transaction_prototype.py
@@ -1,6 +1,7 @@
 from src.abstract.abstract_prototype import AbstractPrototype
 from src.abstract.date_filter_type import DateFilterType
 from src.abstract.period_filter_item import PeriodFilterItem
+from src.exceptions.argument_exception import ArgumentException
 from src.exceptions.operation_exception import OperationException
 from src.logics.filter_item import FilterItem
 from src.logics.warehouse_filter_item import WarehouseFilterItem
@@ -16,7 +17,7 @@ class WarehouseTransactionPrototype(AbstractPrototype):
             DateFilterType.BEFORE: lambda searched_date, date: searched_date.date() > date.date()
         }
 
-    def filter_period(self, field: str, filter_item: PeriodFilterItem) -> list:
+    def __filter_period(self, field: str, filter_item: PeriodFilterItem) -> list:
         if filter_item is None:
             return self.data
         condition = self.__date_conditions.get(filter_item.filter_type) or None
@@ -31,7 +32,7 @@ class WarehouseTransactionPrototype(AbstractPrototype):
                 result.append(item)
         return result
 
-    def filter(self, field: str, filter_item: FilterItem):
+    def __filter(self, field: str, filter_item: FilterItem):
         if filter_item is None:
             return self.data
         condition = self._conditions.get(filter_item.type) or None
@@ -48,11 +49,12 @@ class WarehouseTransactionPrototype(AbstractPrototype):
         return result
 
     def create(self, filter_item: WarehouseFilterItem):
+        ArgumentException.check_arg(filter_item, WarehouseFilterItem)
         if filter_item.warehouse_item:
-            self.data = self.filter("warehouse", filter_item.warehouse_item)
+            self.data = self.__filter("warehouse", filter_item.warehouse_item)
         if filter_item.nomenclature_item:
-            self.data = self.filter("nomenclature", filter_item.nomenclature_item)
+            self.data = self.__filter("nomenclature", filter_item.nomenclature_item)
         if filter_item.period:
-            self.data = self.filter_period("period", filter_item.period)
+            self.data = self.__filter_period("period", filter_item.period)
         prototype = WarehouseTransactionPrototype(self.data)
         return prototype
--- src/services/filter_service.py
+++ src/services/filter_service.py
@@ -1,5 +1,6 @@
 from src.dto.filter_dto import FilterDto
 from src.dto.warehouse_transaction_filter_dto import WarehouseTransactionFilterDto
+from src.exceptions.argument_exception import ArgumentException
 from src.logics.model_prototype import ModelPrototype
 from src.logics.warehouse_transaction_prototype import WarehouseTransactionPrototype
 
@@ -7,12 +8,14 @@ from src.logics.warehouse_transaction_prototype import WarehouseTransactionProto
 class FilterService:
 
     def filter(self, source: list, filter_dto: FilterDto) -> list:
+        ArgumentException.check_arg(filter_dto, FilterDto)
         prototype = ModelPrototype(source)
         for item in filter_dto.items:
             prototype = prototype.create(item)
         return prototype.data
 
     def filter_warehouse_transactions(self, source: list, warehouse_filter_dto: WarehouseTransactionFilterDto) -> list:
+        ArgumentException.check_arg(warehouse_filter_dto, WarehouseTransactionFilterDto)
         prototype = WarehouseTransactionPrototype(source)
         for filter_item in warehouse_filter_dto.items:
             prototype = prototype.create(filter_item)
